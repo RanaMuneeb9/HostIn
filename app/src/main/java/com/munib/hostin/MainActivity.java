ac68175dd7b752547a5467e7e621fcae1711a2a6
musarafique96 <32675005+musarafique96@users.noreply.github.com>
2017-10-10 19:03:01 +0500

    I changed Main activity code to add this functionality

--- app/src/main/java/com/munib/hostin/MainActivity.java
+++ app/src/main/java/com/munib/hostin/MainActivity.java
@@ -13,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         View a= findViewById(R.id.all);
+
+        //Abi icons change nai horae abi me commit krta hn changes or tere pass update kruga to changes ajaegi
+        //ok
+
+        View b=new View();
     }
 }
