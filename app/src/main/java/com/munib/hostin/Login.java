ec30e95a4c8d200eeaa527739d97844ba800cc9e
musarafique96 <32675005+musarafique96@users.noreply.github.com>
2017-10-14 17:59:45 +0500

    navigation updated with login

---
+++ app/src/main/java/com/munib/hostin/Login.java
@@ -0,0 +1,45 @@
+package com.munib.hostin;
+
+import android.content.Intent;
+import android.support.annotation.MainThread;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.FacebookSdk;
+
+public class Login extends AppCompatActivity {
+
+    Button temp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        setContentView(R.layout.login);
+
+        View a= findViewById(R.id.all);
+
+        //
+        Button temp =(Button)findViewById(R.id.temp);
+        temp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
