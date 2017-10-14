ea8af0c261011aa4cc4747c66ad6849c9b4c603c
musarafique96 <32675005+musarafique96@users.noreply.github.com>
2017-10-14 20:33:18 +0500

    signup Activity added

--- app/src/main/java/com/munib/hostin/Login.java
+++ app/src/main/java/com/munib/hostin/Login.java
@@ -1,18 +1,8 @@
 package com.munib.hostin;
 
 import android.content.Intent;
-import android.support.annotation.MainThread;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,7 +14,7 @@ public class Login extends AppCompatActivity {
     Button temp;
     TextView signup;
 
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +38,8 @@ public class Login extends AppCompatActivity {
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.signup_activity);
+                Intent intent =new Intent(Login.this, com.munib.hostin.signup.class);
+                startActivity(intent);
             }
         });
 
